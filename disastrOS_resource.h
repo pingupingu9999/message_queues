959eda75501fd8bd10f2377d9d09279ee2454b4a
pingupingu9999 <100719209+pingupingu9999@users.noreply.github.com>
2022-03-23 18:34:23 +0100

    Added structures and main functions we will need

--- disastrOS_resource.h
+++ disastrOS_resource.h
@@ -12,12 +12,67 @@ typedef struct {
   void* value;
 } Resource;
 
+// struct used to refer to a process that is using a message queue
+typedef struct {
+  ListItem list;
+  int pid;
+  // can be either ready or running 
+  ProcessStatus status;
+} MsgQueueForProcess;
+
+// def to refer to message char*
+typedef char* MsgString;
+
+typedef struct {
+  ListItem list;
+  MsgString msg;
+  int len;
+} Message;
+
+// def to distinguish name across the structures
+typedef ListHead MsgList;
+typedef ListHead MsgQueueList;
+
+typedef struct {
+  // list of message currently in the queue
+  MsgList messages;
+  MsgQueueList processReaders;
+  MsgQueueList processWriters;
+  MsgQueueList non_block;
+  
+  // the largest number of messages queue can afford
+  int maxMsgQueue;
+  // the largest size number a message queue can afford 
+  int maxSizeMsgQueue;
+  // quantity of processes using the queue
+  int nMsgQueueProcesses;
+
+} MsgQueue;
+
 typedef ListHead ResourceList;
 
 // Resource section
 void Resource_init();
 Resource* Resource_alloc(int id, int type);
 int Resource_free(Resource* resource);
+
+// MsgQueue main function for SLAB
+void MsgQueue_init();
+MsgQueue* MsqQueue_alloc();
+int MsqQueue_add_pid(MsgQueue* q, int pid, int mode, ListItem** ds);
+int MsqQueue_free(MsgQueue* queue);
+
+// MsgQueueForProcess main function for SLAB
+void MsgQueueForProcess_init();
+MsgQueueForProcess* MsgQueueForProcess_alloc(int pid);
+int MsgQueueForProcess_free(MsgQueueForProcess* msgQuFProcess);
+
+// Message main function for SLAB
+void Message_init();
+Message* Message_alloc(char* message, int message_len);
+void MsgString_init();
+MsgString MsgString_alloc();
+int Message_free(Message* msg);
 
 // Debug
 Resource* ResourceList_byId(ResourceList* l, int id);
